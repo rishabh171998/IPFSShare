0cf1eb2273635034b91f61669b1c48f05baaa7e9
rishabh171998 <rishabhbhanot10@gmail.com>
2020-09-10 13:58:25 +0530

    Update app.js

--- PNod/app.js
+++ PNod/app.js
@@ -1,17 +1,29 @@
 const util=require('util');
-const Node=require('./Nodes/createNode')
+const Node=require('./Nodes/createNode');
+const pubsubChat=require('./class/chat');
 const connec=async () => {
-  const node1 = await Node.nodeCreate()
-  node1.connectionManager.on('peer:connect', async (connection) => {
-    console.log('Connection established to:', connection.remotePeer.toB58String())	
-// Emitted when a new connection has been created
-  })
-  node1.on('peer:discovery', async (peerId) => {console.log('Discovered:', peerId.toB58String())
-  })
-    await node1.start()
-     node1.pubsub.on('MyApp', (data) => {
-      console.log(data.data.toString());
+  const node = await Node.nodeCreate()
+    await node.start()
+    const pub=new pubsubChat(node,pubsubChat.TOPIC,({from,message})=>
+    {
+      let fromMe=from===node.peerId.toB58String();
+      let user = from.substring(0, 6)
+    if (pub.userHandles.has(from)) {
+      user = pub.userHandles.get(from)
+    }
+    console.info(`${fromMe ? pubsubChat.CLEARLINE : ''}${user}(${new Date(message.created).toLocaleTimeString()}): ${message.data.toString()}`)
+    }) 
+    process.stdin.on('data', async (message) => {
+      // Remove trailing newline
+      message = message.slice(0, -1)
+      // If there was a command, exit early
+      try {
+        // Publish the message
+        await pub.send(message)
+      } catch (err) {
+
+        console.error('Could not publish chat', err)
+      }
     })
-    await node1.pubsub.subscribe('MyApp')
 }
 connec()
